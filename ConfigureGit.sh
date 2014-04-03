f82ccfdb5db78ab393bfe65eee4d8a0a44160561
Philippe Nguyen <pnguye41@calpoly.ed>
2014-04-03 15:02:11 -0700

    track Config script and pledge

---
+++ ConfigureGit.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git config --global user.name "Philippe Nguyen"
+git config --global user.email "pnguye41@calpoly.ed"
+git config --global color.ui "auto"
+git config --global core.editor "nano"
